a3b7b4f8cce149635a444bbc6e989b49abd0a541
Randhir Singh <rick.rox10@gmail.com>
2016-03-15 00:56:18 +0530

    Nothing much

--- CloudBoost/CloudBoost.h
+++ CloudBoost/CloudBoost.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
+
 //! Project version number for CloudBoost.
 FOUNDATION_EXPORT double CloudBoostVersionNumber;
 
---
+++ CloudBoost/CloudBoost.swift
@@ -0,0 +1,23 @@
+//
+//  CloudBoost.swift
+//  CloudBoost
+//
+//  Created by Randhir Singh on 15/03/16.
+//  Copyright © 2016 Randhir Singh. All rights reserved.
+//
+
+import Foundation
+
+public class CloudBoost {
+    var appID: String?
+    var appKey: String?
+    
+    public init(appID: String, appKey: String){
+        self.appID = appID
+        self.appKey = appKey
+    }
+    
+    public func printAppdetails(){
+        print("App ID: " + appID! + "\nApp Key: " + appKey!)
+    }
+}
